dc13b5f347b12fa200097a05d3c5dc09dfb87375
Felipe <ficechin@hotmail.com>
2021-12-17 12:04:05 -0300

    API implemented for all models.

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -5,7 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Cliente extends Model
-{
+class Cliente extends Model {
     use HasFactory;
+
+    protected $fillable = ['nome'];
+
+    public function rules() {
+        return [
+            'nome' => 'required'
+        ];
+    }
 }
