3bbc9438f3194346280f4295336fae247bbd30f7
Felipe <ficechin@hotmail.com>
2021-12-17 12:33:55 -0300

    JWT token implementation in progress.

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller {
+    public function login(Request $request) {
+        $credenciais = $request->all(['email', 'password']);
+        //email = ficechin@hotmail.com - senha = 1234
+        $token = auth('api')->attempt($credenciais);
+        if ($token) {
+            return response()->json(['token' => $token]);
+        } else {
+            return response()->json(['erro' => 'Usuário ou senha inválido.'], 403);
+        }
+    }
+
+    public function logout() {
+        return 'logout';
+    }
+
+    public function refresh() {
+        return 'refresh';
+    }
+
+    public function me() {
+        return 'me';
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CarroController;
 use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\LocacaoController;
@@ -29,4 +30,9 @@ Route::apiResource('carro', CarroController::class);
 Route::apiResource('locacao', LocacaoController::class);
 Route::apiResource('marca', MarcaController::class);
 Route::apiResource('modelo', ModeloController::class);
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout']);
+Route::post('refresh', [AuthController::class, 'refresh']);
+Route::post('me', [AuthController::class, 'me']);
 
